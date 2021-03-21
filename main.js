943082c90dfde2a120e427b41f1c4a9c1227861f
shivansh payak <75551006+151616@users.noreply.github.com>
2021-03-21 06:35:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+
+
+var canvas=new fabric.canvas("myCanvas");
+player_x = 10;
+player_y = 10;
+
+block_image_width = 30;
+block_image_height = 30;
+
+var player_object= "";
+var block_image_object= "";
+
+function player_update()
+{
+	fabric.Image.fromURL("hulk.png", function(Img) {})
+}
+function new_image(get_image){
+    
+}
